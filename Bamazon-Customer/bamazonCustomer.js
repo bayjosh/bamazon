c61a58a6ce3ec0f1965016362650506aec583acc
Josh Bay <joshbay@dhcp-10-105-92-251.wireless.northwestern.private>
2018-03-08 11:45:54 -0600

    updating customer view

--- Bamazon-Customer/bamazonCustomer.js
+++ Bamazon-Customer/bamazonCustomer.js
@@ -5,29 +5,82 @@ var cTable = require('console.table');
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "",
     database: "bamazon_DB"
-  });
+});
 
-  connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
 
     afterConnection();
 });
 
 function afterConnection() {
-    connection.query("SELECT item_id, product_name, price FROM products", function(err, res) {
+    connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
         if (err) throw err;
-        console.log("");
-        console.table(res);
-        console.log("");
+        // console.log("");
+        // console.table(res);
+        // console.log("");
+        productSelect();
+
+    });
+}
+
+function productSelect() {
+    connection.query("SELECT product_name, item_id, price, stock_quantity FROM products", function (err, results) {
+        if (err) throw err;
+        var choiceArray = [];
+        inquirer.prompt([
+                {
+                    name: "productSelect",
+                    type: "rawlist",
+                    choices: function () {
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].product_name + " | $" + results[i].price);
+                        }
+                        return choiceArray;
+
+                    },
+                    message: "Which product would you like to buy? (Use arrows to select)",
+                    pageSize: 20,
+                },
+            ])
+            .then(function (answer) {
+                // get the information of the chosen item
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (choiceArray.indexOf(answer.productSelect) === results[i].item_id) {
+                        chosenItem = results[i];
+                    }
+                }
+                inquirer.prompt([
+                    {
+                        name: "productUnits",
+                        type: "input",
+                        message: "How many of those babies would you like to buy, hun?",
+                        validate: function (value) {
+                            if (isNaN(value) === false && value <= chosenItem.stock_quantity) {
+                                console.log("coming right up!");
+                                return true;
+                            } else if (isNaN(value) === false && value > chosenItem.stock_quantity) {
+                                console.log("\nI can't give ya that many, babe! We only have " + chosenItem.stock_quantity + " left in stock");
+                                return false;
+                            } else {
+                                console.log("\nIt's gotta be a number, darlin'!")
+                                return false;
+                            }
+                        }
+
+                    }
+                ])
+            })
+
         connection.end();
-        
-      });
-  }
+    })
+}
 
